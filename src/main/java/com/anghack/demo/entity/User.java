fac7962823ab90b5342dc78a70527195fcb82585
Angelo Pareto <angelopareto98@gmail.com>
2023-04-05 22:11:19 +0300

    add entity User and config application.properties

---
+++ src/main/java/com/anghack/demo/entity/User.java
@@ -0,0 +1,31 @@
+
+package com.anghack.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author anghack
+ */
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String password;
+}
