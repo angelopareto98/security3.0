5d7c2ba9d178e93c81cfca5050791519d6c59f2c
Angelo Pareto <angelopareto98@gmail.com>
2023-04-06 23:32:20 +0300

    create squelette controller for the registration and authentication

---
+++ src/main/java/com/anghack/demo/controller/auth/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.anghack.demo.controller.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author anghack
+ */
+@RestController
+@RequestMapping(path = "/api/v2/auth")
+public class AuthenticationController {
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        //
+    }
+
+    @PostMapping(path = "/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request){
+        //
+    }
+
+}
