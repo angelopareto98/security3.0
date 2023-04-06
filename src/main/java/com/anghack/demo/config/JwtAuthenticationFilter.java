6c6fd5952b861eefcf78ff4014636b9ea4366be2
Angelo Pareto <angelopareto98@gmail.com>
2023-04-06 21:12:26 +0300

    add dependancies 'jsonwebtoke (jjwt-api, jjwt-impl, jjwt-jackson)'

--- src/main/java/com/anghack/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/anghack/demo/config/JwtAuthenticationFilter.java
@@ -17,11 +17,14 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull  HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
         
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
@@ -29,7 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
         }
         
         jwt = authHeader.substring(7);
-        
+        userEmail = jwtService.extractUsername(jwt);
     }
 
 }
