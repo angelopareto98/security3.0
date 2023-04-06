0684d323e5dbd7fc5fbeb21b34758c185bfbf569
Angelo Pareto <angelopareto98@gmail.com>
2023-04-06 22:19:59 +0300

    create method to check if the token is Valid, the token not expired

--- src/main/java/com/anghack/demo/config/JwtService.java
+++ src/main/java/com/anghack/demo/config/JwtService.java
@@ -45,6 +45,12 @@ public class JwtService {
                 .compact();
     }
     
+   public boolean isTokenValid(String token, UserDetails userDetails){
+       final String username = extractUsername(token);
+       
+       return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+   }
+    
     private Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
@@ -58,6 +64,14 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+       return extractClaim(token, Claims::getExpiration);
     }
 
 }
