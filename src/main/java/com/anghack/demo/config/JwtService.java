074fc511bcfbc210ffc388a4895974490026662b
Angelo Pareto <angelopareto98@gmail.com>
2023-04-06 21:25:37 +0300

    create method to extract all claims from the token in JwtService

---
+++ src/main/java/com/anghack/demo/config/JwtService.java
@@ -0,0 +1,16 @@
+package com.anghack.demo.config;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author anghack
+ */
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+}
